5eb49a8bc3ddc0f8577ea8507248bc97c4e11a29
RushKingdom <140880750+RushKingdom@users.noreply.github.com>
2023-08-27 17:19:55 +0200

    RKClient Website Beta v1
    RKClient Website Beta v1

---
+++ script.js
@@ -0,0 +1,10 @@
+function showAutomaticAlert() {
+    alert("Willkommen auf unserer Webseite!");
+  }
+
+  function scrollToTop() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+  }
